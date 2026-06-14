e61030e30b3c80e06e53a1bfe69354504ee6b482
afi <afnancheranthodika@gmailcom>
2026-06-15 00:28:48 +0530

    Create public store page

--- src/app/(store)/store/[slug]/page.tsx
+++ src/app/(store)/store/[slug]/page.tsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import Store from "@/models/Store";
+import Vehicle from "@/models/Vehicle";
+import { connectDB } from "@/lib/mongodb";
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
+export default async function StorePage(
+  {
+    params,
+  }: {
+    params:
+      Promise<{ slug:string }>
+  }
+) {
+  const { slug } = await params;
+  await connectDB();
+
+
+const store = await Store.findOne({ slug });
+if (!store) {
+ return (
+   <div>
+     Store Not Found
+   </div>
+ );
 }
+  const vehicles = await Vehicle.find({ storeId: store._id }).sort({ createdAt: -1 });
 
-export default page
+  if( vehicles.length === 0 ) {
+    return (
+      <div>
+        <h1>No vehicles available, pleae create some!</h1>
+      </div>
+    )
+  };
+
+  
+  return (
+ <div>
+
+   <h1>{store.name}</h1>
+<p>{store.description}</p>
+
+   <div>
+
+  {vehicles.map(
+   (vehicle:any) => (
+
+    <div key={vehicle._id}>
+      <h3>{vehicle.title}</h3>
+      <p>₹{vehicle.price}</p>
+    </div>
+   ))}
+</div>
+ </div>
+)
+}
